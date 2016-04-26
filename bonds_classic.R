30de81db2400e6eaf356778d5b3653ae4b2b55f1
nazarov <nazarov@corp.sputnik.ru>
2016-04-26 15:49:59 +0300

    bonds

---
+++ bonds_classic.R
@@ -0,0 +1,109 @@
+
+#!/usr/bin/env Rscript
+#Вычисляем лучшую стратегию для пятничных цен закрытия для разных стран
+#
+#Считаем ID по недельным ценам закрытия, а не месячным!!!
+#
+
+rm(list=ls())
+library(parallel)
+if(!require(XLConnect)){
+    install.packages("XLConnect")
+    library(XLConnect)
+}
+source("~/workdir/reality_func2.R")
+RESULT_PATH <- "~/workdir/"
+rankingFactor <- 0
+
+#############################################################################
+# Параметры, которые зависят от изучаемой страны
+country_name_eng <- "russia_bonds"
+
+#############################################################################
+# Загрузка 
+
+price_d5<- readWorksheet(loadWorkbook("data/bonds.xls"),sheet=1)
+
+row.names(price_d5) <- price_d5[,1]
+price_d5 <-price_d5[,-1]
+#############################################################################
+# Создаем тестовое множество
+#per_learn <- 1
+#for_test <- list(price_d5, per_learn*nrow(price_d5) )
+#price_d5  <- price_d5[1:floor(per_learn*nrow(price_d5)),]
+
+#############################################################################
+# Константы
+# перебор от 1 до 12 мес , ждем от 0 до 8 недель, держим от 1 до 24 мес
+# таким образом  последние 25 месяцев понадобятся только для одной модели
+# Шаг для подсчета разниц в группах победителей и проигравших
+STEP=1
+# Периоды отбора (в месяцах), удержания (в неделях), инвестирования (в месяцах)
+UP1=12
+UP2=8
+UP3=12
+#N_test <- (nrow(for_test[[1]])-(2+UP3*4))%/%STEP 
+N <- (nrow(price_d5)-(2+UP3*4))%/%STEP 
+
+#############################################################################
+temp_for_T <-  returnWrapper(4, 0, 4, STEP, N, price_d5, UP1, UP2, 0.1, 0) 
+T <- length(temp_for_T)
+
+# N - с учетом отступа
+# n <- T-R+1
+#############################################################################
+# Процедура формирования портфелей, подсчета статистик
+start_time <- Sys.time()
+
+resultDataFull <- price_d5
+
+print("Параллельное выполнение")
+cl <- makeCluster(getOption("cl.cores", 4)) # создание кластера из четырёх ядер процессора
+clusterExport(cl,"infert") # передача данных внутрь кластера
+clusterEvalQ(cl,source("~/workdir/reality_func2.R")) # загрузка функций в кластер
+#clusterExport(cl, "UP1", "UP2", "UP3", "STEP", "resultDataFull", "N")
+start_time <- Sys.time()
+temp1 <- parLapply(cl,  1:4, function(temp_p3, UP1, UP2, UP3, STEP, resultDataFull, N, rankingFactor) # параллельная версия sapply
+{    m <- 1  
+#realityCheckData <- data.frame(1,1,1,1,1,1,1,1,1,1)
+realityCheckData <- data.frame(1,1,1,1,1,1,1,1)
+low <- (temp_p3-1)*3+1
+up <- temp_p3*3
+for (p3 in low:up) {  
+  for (percent in c(0.1,0.3,0.4,0.5) ){
+    for (p1 in 1:UP1 ){   
+      for (p2 in 0:UP2 ){  
+        #вектор дельт   
+        cat(p1,p2,p3, "/n")
+        temp <- returnWrapper(p1, p2, p3, STEP, N, resultDataFull, UP1, UP2, percent, rankingFactor) 
+        #return.winner<- ret.winner(p1, p2, p3, STEP, N, resultDataFull, UP1, UP2, percent) 
+        #return.loser<- ret.loser(p1, p2, p3, STEP, N, resultDataFull, UP1, UP2, percent) 
+        n <- length(temp)
+        #realityCheckData[m, ] <- list(mean(temp),abs(mean(temp))/sd(temp)*sqrt(n), (1-pt(q = abs(mean(temp))/sd(temp)*sqrt(n),df = n-1))*2 ,p1*4, p2, p3*4, percent,
+        #                              mean(return.winner), mean(return.loser),length(temp[temp<0]))    
+         realityCheckData[m, ] <- list(mean(temp),abs(mean(temp))/sd(temp)*sqrt(n), (1-pt(q = abs(mean(temp))/sd(temp)*sqrt(n),df = n-1))*2 ,p1*4, p2, p3*4, percent,
+                              length(temp[temp<0]))  
+        m <- m+1      
+      }
+    }
+  }       
+}
+return (realityCheckData)
+
+}, UP1, UP2, UP3, STEP, resultDataFull, N, rankingFactor)
+stopCluster(cl)
+
+end_time <- Sys.time()
+end_time
+
+temp2 <-do.call("rbind", temp1)
+colnames(temp2) <-c("mean","t","p-value","hist_per","moment_per","invest_per","percent", "Amount_of_negative")
+
+##################################################################
+#Сохранение результатов
+results <- list(data=temp2, num=N, n_portf = T)  # список ценных объектов
+
+saveRDS(file = paste(RESULT_PATH ,"result", "_",country_name_eng,"_f",rankingFactor,"_",Sys.time() ,".RDS",sep=""),results) # сохраняем всё ценное в файл
+
+start_time
+end_time
--- reality_func2.R
+++ reality_func2.R
@@ -1,7 +1,10 @@
 
 
 
-returnWrapper <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent, flag){             
+returnWrapper <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent, flag){ 
+                if(flag==0){
+                           return (ret_0(p1, p2, p3, STEP, N, d2, UP1, UP2, percent))
+                }
                 if(flag==1){
                            return (ret_1(p1, p2, p3, STEP, N, d2, UP1, UP2, percent))
                 }
@@ -9,13 +12,17 @@ returnWrapper <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent, flag){
                   return (ret_2(p1, p2, p3, STEP, N, d2, UP1, UP2, percent))
                 }
                 if(flag==3){
-                  return (ret_3(p1, p2, p3, STEP, N, d2, UP1, UP2, percent))
+                  i_start <- STEP
+                  return (ret_3(p1, p2, p3, i_start , N, d2, UP1, UP2, percent))
                 }
 
 }
 
 rankingFactorWrapper <- function(p1, p2, p3, STEP, N, d2, i, flag){
 #Обертка для факторов ранжирования компаний
+                if(flag==0){
+                           return (rankingFactor_0(p1, p2, p3, STEP, N, d2, i))
+                }
                 if(flag==1){
                            return (rankingFactor_1(p1, p2, p3, STEP, N, d2, i))
                 }
@@ -42,7 +49,44 @@ id_coeff <- function (datatable, p1, p2, i) {
           return(sgn*sum_signs_daily_returns)
 }
 
-
+rankingFactor_0 <- function(p1, p2, p3, STEP, N, d2, i){
+  # Выдает датафрейм , в котором столбцы расставлены в порядке, определяемым
+  # ранжирующей функцией - просто - победители и проигравшие - по доходности, без
+  # дополнительных отборов
+  #
+  # Args:
+  #   p1: исторический период
+  #   p2: период ожидания
+  #   p3: инвестиционный период
+  #   STEP:  Шаг для подсчета разниц в группах победителей и проигравших
+  #   d2: исходная  таблица с ценами закрытия
+  #   i: момент времени
+  #
+  # Returns:
+  #   Исходная таблица со столбцами в определенном порядке data.frame
+  
+  # Удаляем нулевые столбцы
+  d <- d2[,d2[i-4*p1-p2,]!=0 & d2[i,]!=0 & d2[i-p2,]!=0]
+  temp1 <- (as.numeric(d[i-p2,]) - as.numeric(d[i-4*p1-p2,]))/as.numeric(d[i-4*p1-p2,])
+  #temp1 <- ( t(d)[,i-p2] - t(d)[,i-4*p1-p2 ]) / t(d)[,i-4*p1-p2]
+  temp2 <- d[,order(-temp1)]
+  #######################################################################################################   
+  # Для случая нахождения нулей в середине столбца с ценами закрытия - меняем ноль на последний ненулевой   
+  for(k in 1:ncol(temp2)){
+    if(temp2[i+p3*4, k]==0){
+      
+      count<-1
+      while(temp2[i+p3*4-count,k]==0){
+        count<-count+1
+      }
+      temp2[i+p3*4, k] <- temp2[i+p3*4-count, k] 
+      
+    }
+  }
+  #######################################################################################################  
+  #data.frame(..., check.names = FALSE)
+  return (temp2) 
+}
 rankingFactor_1 <- function(p1, p2, p3, STEP, N, d2, i){
 # Выдает датафрейм , в котором столбцы расставлены в порядке, определяемым
 # ранжирующей функцией - сначала две группы - победители и проигравшие - по доходности, далее
@@ -90,7 +134,44 @@ rankingFactor_1 <- function(p1, p2, p3, STEP, N, d2, i){
           return (temp2) 
 }
 
-
+ret_0 <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent)
+{
+          #средняя месячная доходность - классическая схема
+          ans <- c() 
+          i <- UP1*4+2+UP2
+          m <- 1 
+          while(i < N){
+                        temp2 <- rankingFactorWrapper(p1, p2, p3, STEP, N, d2, i, 0)
+                        temp3 <- (as.numeric(temp2[i+p3*4,])- as.numeric(temp2[i,]))/as.numeric(temp2[i,])
+                        
+                        if(percent==0.5){
+                                    ret_inv <- sum(temp3[1:floor(length(temp3)*percent)] )/ floor(length(temp3)*percent)- 
+                                      sum(temp3[(floor(length(temp3)*percent)+1):(length(temp3))]) /(length(temp3)
+                                                                                                     -floor(length(temp3)*percent))
+                                    #ans[m] <- (1 + ret_inv)^(1/p3) - 1
+                                    ans[m] <- (sum(temp3[(floor(length(temp3)*percent)+1):(length(temp3))]) /
+                                                 (length(temp3)-floor(length(temp3)*percent)))/p3
+                        }
+                        else{
+                                    ret_inv <- sum(temp3[1:ceiling(length(temp3)*percent)])/ceiling(length(temp3)*percent)- 
+                                      sum(temp3[ceiling(length(temp3)*(1-percent)):(length(temp3))])/ceiling(length(temp3)*percent)
+                                    #ans[m] <- (1 + ret_inv)^(1/p3) - 1 
+                                    ans[m] <- (sum(temp3[ceiling(length(temp3)*(1-percent)):(length(temp3))]) /ceiling(length(temp3)*percent))/p3
+                                    #средняя месячная доходность =(1+доходность за n месяцев)^(1/n)-1
+                        }
+                        
+                        #if(m==312){
+                        #            print(temp3)
+                        #            print (ret_inv)
+                        #            print (ans[m])
+                        # }
+                        
+                        names(ans)[m] <- row.names(d2)[i]    
+                        m <- m+1
+                        i<-STEP+i  
+          }   
+          return(ans)
+}
 ret_1 <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent)
 {
           # набор дельт  - ans, считает через корень доходность
@@ -129,7 +210,7 @@ ret_1 <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent)
 }
 ret_2 <- function (p1, p2, p3, STEP, N, d2, UP1, UP2, percent)
 {
-          # набор дельт  - ans, считает через корень доходность
+          # набор дельт  - ans, rankFactor = 1
           #средняя месячная доходность - классическая схема
           ans <- c() 
           i <- UP1*4+2+UP2
